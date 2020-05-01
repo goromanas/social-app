0103597f4dcab26aa686589187e57ccad3481b97
Romanas Golovko <ro@Romanass-MBP.lan>
2020-05-02 01:30:22 +0300

    Finished register and login functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,47 @@
 const User = require('../models/User');
 
-exports.login = function() {
-
+exports.login = function(req, res) {
+    let user = new User(req.body);
+    user.login().then(function(result) {
+        req.session.user = {username: user.data.username, avatar: user.avatar}
+        req.session.save(function() {
+            res.redirect('/');
+        });
+    }).catch(function(e) {
+        req.flash('errors', e);
+        req.session.save(function() {
+            res.redirect('/');
+        })
+    });
 }
 
-exports.logout = function() {
-    
+exports.logout = function(req, res) {
+    req.session.destroy(function() {
+        res.redirect('/');
+    });
 }
 
 exports.register = function(req, res) {
     let user = new User(req.body);
-    user.register();
-    if (user.errors.length) {
-        res.send(user.errors);
-    } else {
-        res.send('COngrats! There are no errors');
-    }
+    user.register().then(() => {
+       req.session.user = {username: user.data.username, avatar: user.avatar};
+       req.session.save(function() {
+        res.redirect('/');
+     })
+    }).catch((regErrors) => {
+        regErrors.forEach(function(error) {
+            req.flash('regErrors', error);
+        })
+        req.session.save(function() {
+            res.redirect('/');
+        })
+    }) 
 }
 
 exports.home = function(req, res) {
-    res.render('home-guest');
+    if (req.session.user) {
+        res.render('home-dashboard', {username: req.session.user.username, avatar: req.session.user.avatar});
+    } else {
+        res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')});
+    }
 }
--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongodb = require('mongodb');
 
 
 mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
-    module.exports = client.db();
+    module.exports = client;
     const app = require('./app');
     app.listen(process.env.PORT);
 });
